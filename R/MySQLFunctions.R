989da8f953810b254cb7955eb531ad46f239c838
Adam <a.ghneim@gmail.com>
2018-12-07 10:04:47 +0100

    Ignore patients that have 0 values on variables

--- R/MySQLFunctions.R
+++ R/MySQLFunctions.R
@@ -1,7 +1,7 @@
 ImportMangroveMySQL <- function(mysql.server.name, mysql.server.port, mysql.database, mysql.username, mysql.password, mysql.Mangrove.table = "2012_summary") {
     mydb <- dbConnect(MySQL(), user = mysql.username, password = mysql.password, dbname = mysql.database, host = mysql.server.name, port = mysql.server.port)
     ## Select all data from database
-    study.data <- dbGetQuery(mydb, sprintf("select * from %s", mysql.Mangrove.table))
+    study.data <- dbGetQuery(mydb, sprintf("select * from %s where SBP > 0 and PULSE>0 and RR>0 and GCSTOT>0", mysql.Mangrove.table))
     dbDisconnect(mydb) ## Disconnect from database
     study.data$SBP <- as.numeric(study.data$SBP)
     study.data$PULSE <- as.numeric(study.data$PULSE)
